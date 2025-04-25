30398ce6e95c421261439ae1086782e0d837e035
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-04-25 23:33:17 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useEffect, useState } from "react";
+import "./App.css";
+import WeatherInfo from "./components/Weather";
+
+const popularCities = [
+  "Karachi",
+  "Tokyo",
+  "New York",
+  "Paris",
+  "London",
+  "Shanghai",
+  "Los Angeles",
+  "Beijing",
+  "Mumbai",
+  "Moscow",
+  "Hong Kong",
+  "São Paulo",
+  "Dubai",
+  "Singapore",
+  "Bangkok",
+  "Istanbul",
+  "Sydney",
+  "Seoul",
+  "Mexico City",
+  "Berlin",
+  "Madrid",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chosen, setChosen] = useState(popularCities[0]);
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${chosen}&appid=1f136667cfcdb418bf8b7a4c5a542f00`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setWeatherData(res);
+        setLoading(false);
+      });
+  }, [chosen]);
+
+  const handleCityChange = (e) => {
+    setChosen(e.target.value);
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center">
+      <h1 className="text-3xl text-gray-800 font-bold mb-10">Weather App</h1>
+      <select
+        onChange={handleCityChange}
+        value={chosen}
+        className="p-3 mb-5 border rounded bg-white text-black"
+      >
+        {popularCities.map((data, ind) => (
+          <option key={ind} value={data}>
+            {data}
+          </option>
+        ))}
+      </select>
+
+      {!weatherData ? (
+        <div className="h-96 flex justify-center items-center">
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <WeatherInfo weatherData={weatherData} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//      <h1>Weather App</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+
